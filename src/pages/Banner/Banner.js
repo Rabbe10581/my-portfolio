9bafe5ae7b7c08e9682ac1476785fca87250f85a
Rabbe <onikmahmudrabbe@gmail.com>
2022-12-11 04:12:14 +0600

    Banner added

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import image from '../../Assets/Banner/my-bg.png'
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className="hero bg-slate-400">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={image} className=" lg:w-1/2 rounded-lg shadow-2xl" />
+                <div className='w-1/2'>
+                    <h1 className="lg:text-5xl font-bold">Hello I am <br />Md Fazla Rabbe Anik</h1>
+                    <p className="py-6">As a Web Developer, I have experience of a year with MERN developing and I am highly dedicated and hard-working.</p>
+                    <button className="btn btn-primary">View Resume</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import Header from '../../Shared/Header/Header';
-// import Navbar from '../../Shared/Navbar/Navbar';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
 import Projects from '../Projects/Projects';
