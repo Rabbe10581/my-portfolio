563b1636adac24f6aeead4ca8f70b0c506d14c1b
Rabbe <onikmahmudrabbe@gmail.com>
2022-12-10 23:25:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Shared/Navbar/Navbar';
+import Banner from './pages/Banner/Banner';
+import Skills from './pages/Skills/Skills';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
+import Footer from './Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
