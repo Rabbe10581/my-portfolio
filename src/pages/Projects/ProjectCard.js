e428ef01d4a55f455820d87866333fe344561b11
Rabbe <onikmahmudrabbe@gmail.com>
2022-12-12 01:05:16 +0600

    card changed projects

--- src/pages/Projects/ProjectCard.js
+++ src/pages/Projects/ProjectCard.js
@@ -3,16 +3,14 @@ import React from 'react';
 const ProjectCard = ({ project }) => {
     const { projects_name, technologies, features, image } = project;
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl lg:mx-10">
-            <figure className='lg:w-1/2'><img src={image} alt="Album" /></figure>
+        <div className="card card-compact w-fit bg-base-200 shadow-xl">
+            <figure><img src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{projects_name}</h2>
-                <ol className='lg:text-1xl'>
-                    <li>1. Three roles can participate. Admin, Buyer & Seller.</li>
-                    <li>2. Admin can see all buyers, and sellers can handle them.</li>
-                    <li>3. Buyers can buy and see orders, and sellers can upload and see products.</li>
-                </ol>
-                <p className='lg:text-2xl'>{technologies}</p>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Explore More</button>
+                </div>
             </div>
         </div>
     );
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -6,9 +6,9 @@ const Projects = () => {
     const projects = useLoaderData()
     console.log(projects);
     return (
-        <div className='bg-slate-200'>
+        <div className='bg-slate-200 py-10 my-6'>
             <h2 className='text-3xl font-serif font-semibold text-center'>My Projects</h2>
-            <div className='grid gap-6 grid-cols-1 py-6'>
+            <div className='grid gap-4 lg:gap-10 grid-cols-1 lg:grid-cols-3 py-6 mx-2 lg:mx-6'>
                 {
                     projects.map(project => <ProjectCard
                         key={project.id}
